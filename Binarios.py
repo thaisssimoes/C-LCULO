c4ab0b3f683c291e62c0cae0db73475c22581811
Thais Simões <Thais Simes>
2017-01-09 14:56:27 -0200

    Primeiro commit

---
+++ Binarios.py
@@ -0,0 +1,18 @@
+class numero_binario:
+    def calculo(self, num):
+        self.lista_numeros=[]
+        while num !=0:
+            self.numero = num%2
+            self.lista_numeros.append(self.numero)
+            num = num/2
+
+    def imprime(self):
+        for i in reversed(self.lista_numeros):
+            print i,
+        print("\n")
+
+
+numero = numero_binario()
+for num in range(100):
+    numero.calculo(num)
+    numero.imprime()
